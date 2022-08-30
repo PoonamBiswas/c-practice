70695407e041ad171e9d178c85b1071cb5d88b2a
PoonamBiswas <90602111+PoonamBiswas@users.noreply.github.com>
2022-08-30 11:08:45 +0530

    Add files via upload
    This file contains c programming related to arrays.

--- main.c
+++ main.c
@@ -1,52 +1,61 @@
 #include <stdio.h>
 
-int main()
-{
-    char type;
-    float enter, left, enterhr, lefthr, entermin, leftmin, amount, stay;
-    printf("ENTER YOUR VEHICLE TYPE : \n\tTruck(T)\n\tBus(B)\n\tCar(C)\n\tTwo wheelers(M)\n\t::");
-    scanf("%c",&type);
-    printf("\nAT WHAT TIME YOU ENTERED THE PARKING LOT: (HOUR(1-12):MINUTE(1-60): ");
-    scanf("%f%f",&enterhr,&entermin);
-    printf("\nAT WHAT TIME YOU LEFT THE PARKING LOT: (HOUR(1-12):MINUTE(1-60): ");
-    scanf("%f%f",&lefthr,&leftmin);
-    enter = enterhr + entermin/60;
-    left = lefthr + leftmin/60;
-    stay = left - enter;
-    printf("\nYOUR VEHICLE STAYED FOR %f HOUR IN OUR PARKING LOT",stay);
-    switch(type)
-    {
-        case'T':
-            if(stay<=3)
-                amount = stay*20;
-            else
-                amount = stay*30;
-        printf("\nAMOUNT TO BE PAID :%f rs", amount);
-        break;
-        case'B':
-            if(stay<=3)
-                amount = stay*20;
-            else
-                amount = stay*30;
-        printf("\nAMOUNT TO BE PAID :%f rs", amount);
-        break;
-        case'C':
-            if(stay<=3)
-                amount = stay*10;
-            else
-                amount = stay*20;
-        printf("\nAMOUNT TO BE PAID :%f rs", amount);
-        break;
-        case'M':
-            if(stay<=3)
-                amount = stay*10;
-            else
-                amount = stay*20;
-        printf("\nAMOUNT TO BE PAID :%f rs", amount);
-        break;
-        
-    }
+int main() {
+    int i, pos, n;
+    float x;
+    float marks[100];
     
+    printf("Enter the number of students : ");
+    scanf("%d",&n);
+    printf("Enter marks of students : ");
 
-    return 0;
+    // taking input and storing it in an array
+    for(int i = 0; i < n; ++i) {
+        scanf("%f", &marks[i]);
+  }
+      for(int i = 0; i < n; ++i) {
+        printf("%f\t", marks[i]);
+  }
+        // element to be inserted
+        printf("\n Enter marks to be inserted :");
+        scanf("%f", &x);
+        // position at which element is to be inserted
+        printf("Enetr position of the marks: ");
+        scanf("%d", &pos);
+        n++;
+        // shift elements forward
+        for (i = n-1; i >= pos; i--)
+            marks[i] = marks[i - 1];
+        // insert x at pos
+        marks[pos - 1] = x;
+        // print the updated array
+            for (i = 0; i < n; i++)
+                printf("%f ", marks[i]);
+                printf("\n");
+         // enter the position of the element to be deleted  
+    printf( " Define the position of the marks which you want to delete: \n ");  
+    scanf (" %d", &pos);  
+    // check whether the deletion is possible or not  
+    if (pos >= n+1)  
+    {  
+        printf (" \n Deletion is not possible in the array.");  
+    }  
+    else  
+    {  
+        // use for loop to delete the element and update the index  
+        for (i = pos - 1; i < n -1; i++)  
+        {  
+            marks[i] = marks[i+1]; // assign arr[i+1] to arr[i]  
+        }  
+          
+        printf (" \n The resultant array is: \n"); 
+         // display the final array  
+        for (i = 0; i< n - 1; i++)  
+        {  
+            printf (" marks[%d] = ", i);  
+            printf (" %f \n", marks[i]);  
+        }
+    }
+
+  return 0;
 }
